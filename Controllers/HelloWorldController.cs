b6b1f7fb32837f2d58e58bb80511e3c4b556e019
Hzrj <1316836373@qq.com>
2021-05-01 19:57:19 +0800

    添加视图,打印名字

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -9,19 +9,16 @@ namespace WebApp.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //public IActionResult Index() 
-        //{
-        //    return View();
-        //}
-         
-        public string index() 
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        public string Welcome(string name,int ID=1)
+        public IActionResult Welcome(string name,int NumTimes=1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name},NumTimes is {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = NumTimes;
+            return View();
         }
     }
 }
