fa80a568d76a1bdbb843ee764f6d042a863d0cef
Hzrj <1316836373@qq.com>
2021-05-01 17:48:39 +0800

    地址栏上面加个 /HelloWorld 或者 /HelloWorld/Welcom

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+         
+        public string index() 
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcom()
+        {
+            return "name is ZRJ";
+        }
+    }
+}
