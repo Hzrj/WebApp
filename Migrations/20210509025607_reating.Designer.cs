08bd9ef89733d5b2a79c2fbd37003fac260821ce
Hzrj <1316836373@qq.com>
2021-05-09 11:19:41 +0800

    添加字段 添加验证 https://docs.microsoft.com/zh-cn/aspnet/core/tutorials/first-mvc-app/details?view=aspnetcore-5.0 官网API   本地链接 https://localhost:{port}/Movies

--- Migrations/20210509025607_reating.Designer.cs
+++ Migrations/20210509025607_reating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Data;
 
 namespace WebApp.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20210509025607_reating")]
+    partial class reating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
